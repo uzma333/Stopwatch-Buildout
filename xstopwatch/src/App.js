f81f9c730cf949a97f466fc82e4cc1b49c17229a
noor uzma <uzmasalmaan23@gmail.com>
2024-06-07 07:54:52 +0530

    added functionality for the stopwatch

---
+++ xstopwatch/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Stopwatch from './StopWatch';
+
+function App() {
+  return (
+    <div className="App">
+     <Stopwatch/>
+    </div>
+  );
+}
+
+export default App;
---
+++ xstopwatch/src/StopWatch.jsx
@@ -0,0 +1,66 @@
+import { useEffect, useState,useRef } from "react"
+
+function Stopwatch(){
+    const[sec,setSec]=useState(0);
+    const[min,setMin]=useState(0);
+    const[running,setRunning]=useState(false);
+    const timer=useRef(null);
+
+    
+
+
+
+
+    useEffect(()=>{
+        if(running){
+            timer.current=setInterval(()=>{
+                setSec(prevsec=>{
+                    if(prevsec===59){
+                        setMin(prevMin=>prevMin+1);
+                    return 0;
+                    }
+                    return prevsec+1;
+                })
+            },1000)
+           }else{
+            clearInterval(timer.current)
+           }
+    
+        
+return ()=>{
+    clearInterval(timer.current)
+}
+    },[running])
+
+
+
+
+    const handleStartStop=()=>{
+        setRunning(running=>!running);
+
+    }
+const handleReset=()=>{
+    setRunning(false);
+    setMin(0);
+    setSec(0);
+
+}
+
+
+
+
+
+
+
+
+
+    return(
+        <div>
+            <h2>Stopwatch</h2>
+            <p>time: {min}:{sec <10? "0"+sec:sec}</p>
+            <button onClick={handleStartStop}>{running ? "stop" : "start"}</button>
+            <button onClick={handleReset}>Reset</button>
+        </div>
+    )
+}
+export default Stopwatch
